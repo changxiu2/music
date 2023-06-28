506531911f6b9455ec67e56291835b89dbd67580
常秀 <3298632063@qq.com>
2023-06-28 11:48:08 +0800

    docs(home): 首页
    首页
    
    ISSUES CLOSED: #1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import './index.css';
 import { Icon } from '@iconify/vue2';
 import App from '@/App.vue';
+import { Swipe, SwipeItem } from 'vant';
+
+Vue.use(Swipe);
+Vue.use(SwipeItem);
 Vue.component('Icon', Icon);
 
 const vm = new Vue({
